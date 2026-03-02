58f3b370364a0e06c8588a68ccf7b197c534b46f
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-02 10:51:09 +0100

    feat: add branding and styles to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="text-7xl p-4">
+      <p className="font-rosnoc">orbit</p>
+      <p className="font-google-sans font-medium">By GDG Babcock</p>
+    </div>
+  );
 };
+
 export default App;
