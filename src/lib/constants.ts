0608d198243bf820e798076435d8ebbcc36d7fe7
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-04 16:27:24 +0100

    feat(lib): add new utility module

---
+++ src/lib/constants.ts
@@ -0,0 +1,8 @@
+export const IMAGES = {
+  logo: { src: "/images/svgs/logo.svg", width: 100, height: 100 },
+  sponsors: { src: "/images/svgs/sponsors.svg", width: 21, height: 22 },
+  partners: { src: "/images/svgs/partners.svg", width: 20, height: 20 },
+  rocket: { src: "/images/svgs/rocket.svg", width: 19, height: 19 },
+  schedule: { src: "/images/svgs/schedule.svg", width: 20, height: 20 },
+  speakers: { src: "/images/svgs/speakers.svg", width: 20, height: 17 },
+};
