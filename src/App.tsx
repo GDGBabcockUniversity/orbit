96ea043498fb18b58b3a3c9bbdbb53b095456532
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-04 06:28:09 +0100

    feat: replace App content with HeroSection component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
+import HeroSection from "./components/HeroSection";
+
 const App = () => {
-  return (
-    <div className="text-7xl p-4">
-      <p className="font-rosnoc">orbit</p>
-      <p className="font-google-sans font-medium">By GDG Babcock</p>
-    </div>
-  );
+  return <HeroSection />;
 };
 
 export default App;
