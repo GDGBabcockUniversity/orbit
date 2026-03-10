708063334fc2d5481f1e7c2e3a689829528bfd0f
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-10 15:24:23 +0100

    feat: update home page sections

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,20 @@
 import Hero from "../components/hero";
-import WhatsInItForYou from "../components/whats-in-it";
-import TeamSection from "../components/team-section";
+import ExperienceSection from "../components/experience-section";
+import WhyOrbitSection from "../components/why-orbit-section";
 import SpeakersSection from "../components/speakers-section";
 import StatsSection from "../components/stats-section";
+import SponsorsPreview from "../components/sponsors-preview";
 import TicketsSection from "../components/tickets-section";
 import FAQSection from "../components/faq-section";
 
 const Home = () => (
   <>
     <Hero />
-    <WhatsInItForYou />
-    <TeamSection />
+    <ExperienceSection />
+    <WhyOrbitSection />
     <SpeakersSection />
     <StatsSection />
+    <SponsorsPreview />
     <TicketsSection />
     <FAQSection />
   </>
