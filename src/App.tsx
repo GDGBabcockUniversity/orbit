3e14c1c80294182cf80ab11b936c61562ab37201
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-10 15:23:35 +0100

    feat: add navbar and sponsors page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import SpeakersPage from "./pages/speakers";
+import SponsorsPage from "./pages/sponsors";
 import Footer from "./components/footer";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/speakers" element={<SpeakersPage />} />
+        <Route path="/sponsors" element={<SponsorsPage />} />
       </Routes>
       <Footer />
     </>
