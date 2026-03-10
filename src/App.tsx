9398dc5fb06da2f19b3da9fefeb1f4b6b9ef10b5
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-10 06:22:03 +0100

    feat: add Footer component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import SpeakersPage from "./pages/speakers";
+import Footer from "./components/footer";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/speakers" element={<SpeakersPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/speakers" element={<SpeakersPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
