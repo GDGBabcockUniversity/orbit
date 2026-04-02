42b1b0052ccab9e17a1a1e3a1b96cb5241c5517b
Victor Ibironke <victoribironke2019@gmail.com>
2026-04-02 05:56:33 +0100

    feat: enable raffle route

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SpeakersPage from "./pages/speakers";
 import SponsorsPage from "./pages/sponsors";
 import TicketsPage from "./pages/tickets";
 import AdminPage from "./pages/admin";
-// import RafflePage from "./pages/raffle";
+import RafflePage from "./pages/raffle";
 import AgendaPage from "./pages/agenda";
 import Footer from "./components/footer";
 
@@ -20,7 +20,7 @@ const App = () => {
         <Route path="/agenda" element={<AgendaPage />} />
         <Route path="/sponsors" element={<SponsorsPage />} />
         <Route path="/tickets" element={<TicketsPage />} />
-        {/*<Route path="/raffle" element={<RafflePage />} />*/}
+        <Route path="/raffle" element={<RafflePage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
       <Footer />
